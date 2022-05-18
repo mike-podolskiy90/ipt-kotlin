0b86f66cecda9866da5de47b370c7b7bdc95c098
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2022-05-18 13:45:45 +0200

    Add base project styles, js and images

---
+++ src/main/resources/static/js/global.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $("#showMore").on("click", function (e) {
+        e.preventDefault();
+        var $parentTarget = $(e.target).parent();
+        var showLess = $parentTarget.html();
+        $parentTarget.html($parentTarget.next("#hiddenContent").html());
+        $parentTarget.next("#hiddenContent").html(showLess);
+    });
+    $("#showLess").on("click", function (e) {
+        e.preventDefault();
+        var $parentTarget = $(e.target).parent();
+        var showMore = $parentTarget.html();
+        $parentTarget.html($parentTarget.next("#hiddenContent").html());
+        $parentTarget.next("#hiddenContent").html(showMore);
+    });
+});
+
+function initCalendar(context) {
+    if (context === undefined) context = "";
+
+    $(context + " .calendarInfo").each(function (i) {
+        $(this).find("input").datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            orientation: "bottom",
+            forceParse: false
+        });
+    });
+}
+
+function initInfoPopovers(item) {
+    var popoverTriggerList = [].slice.call(item.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+    try {
+        var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+            trigger: 'focus'
+        })
+    } catch (TypeError) {
+    }
+}
